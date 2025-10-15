e60c1d70ead201f42958880aa2fbf7ec427c7caa
LarviNA <mbarraza193@gmail.com>
2025-10-14 23:53:49 -0300

    Limpieza final: Eliminar paquetes duplicados y obsoletos

--- app/src/main/java/com/example/quintaruta/ui/common/UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.quintaruta.ui.common
-
-
-sealed class UiState<out T> {
-    object Idle : UiState<Nothing>()
-    object Loading : UiState<Nothing>()
-    data class Success<out T>(val data: T) : UiState<T>()
-    data class Error(val message: String) : UiState<Nothing>()
-}

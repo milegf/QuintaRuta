86074b33166c0cf8fbe96cb584de9c4754ea9a29
MILE GUERRA <mi.guerra@duocuc.cl>
2025-10-22 02:12:32 -0300

    feat: configuración de room, nueva versión de coil y correccion de errores

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,12 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose.compiler)
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "com.example.quintaruta"
     compileSdk {
-        version = release(36)
+        compileSdk = 36
     }
 
     defaultConfig {
@@ -60,7 +61,11 @@ dependencies {
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
-    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    implementation("androidx.room:room-runtime:2.8.2")
+    kapt("androidx.room:room-compiler:2.8.2")
+    implementation("androidx.room:room-ktx:2.8.2")
 
     implementation(libs.androidx.compose.runtime.livedata)
     implementation(libs.androidx.compose.material.icons.extended)

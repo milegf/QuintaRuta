34e2865766af00a18b7782ed20beea0cc6ab2d22
MILE GUERRA <mi.guerra@duocuc.cl>
2025-10-22 20:58:13 -0300

    feat: inclusión de trivia al mapper (.toModel y .toEntity)

--- app/src/main/java/com/example/quintaruta/util/Mapper.kt
+++ app/src/main/java/com/example/quintaruta/util/Mapper.kt
@@ -2,9 +2,12 @@ package com.example.quintaruta.util
 
 import com.example.quintaruta.data.local.entity.PoiEntity
 import com.example.quintaruta.data.local.entity.RutaEntity
+import com.example.quintaruta.data.local.entity.TriviaEntity
 import com.example.quintaruta.data.model.Poi
 import com.example.quintaruta.data.model.Ruta
+import com.example.quintaruta.data.model.Trivia
 
+// MAPPERS DE RUTA
 fun RutaEntity.toModel() = Ruta(
     id = id,
     nombre = nombre,
@@ -23,6 +26,7 @@ fun Ruta.toEntity() = RutaEntity(
     creador = creador
 )
 
+// MAPPERS DE POIs
 fun PoiEntity.toModel() = Poi(
     id = id,
     rutaId = rutaId,
@@ -41,4 +45,26 @@ fun Poi.toEntity() = PoiEntity(
     latitud = latitud,
     longitud = longitud,
     imagenUrl = imagenUrl
+)
+
+// MAPPERS DE TRIVIA
+fun TriviaEntity.toModel() = Trivia (
+    id = this.id,
+    poiId = this.poiId,
+    pregunta = this.pregunta,
+    opciones = listOf(this.opA, this.opB, this.opC, this.opD),
+    respuestaCorrecta = this.respuestaCorrecta,
+    respondida = this.respondida
+)
+
+fun Trivia.toEntity() = TriviaEntity(
+    id = this.id,
+    poiId = this.poiId,
+    pregunta = this.pregunta,
+    opA = this.opciones.getOrNull(0) ?: "",
+    opB = this.opciones.getOrNull(1) ?: "",
+    opC = this.opciones.getOrNull(2) ?: "",
+    opD = this.opciones.getOrNull(3) ?: "",
+    respuestaCorrecta = this.respuestaCorrecta,
+    respondida = this.respondida
 )

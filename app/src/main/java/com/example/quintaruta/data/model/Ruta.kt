23d042a2cb33efa8c6370c0f245babdd25c209e2
MILE GUERRA <mi.guerra@duocuc.cl>
2025-10-22 02:18:15 -0300

    feat: creacion modelo: ruta

--- app/src/main/java/com/example/quintaruta/data/model/Ruta.kt
+++ app/src/main/java/com/example/quintaruta/data/model/Ruta.kt
@@ -1,2 +1,15 @@
-package com.example.quintaruta.data.model
+package com.quintaruta.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "rutas")
+data class Ruta(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val nombre: String,
+    val descripcion: String,
+    val categoria: String,
+    val imagenUrl: String? = null
+)

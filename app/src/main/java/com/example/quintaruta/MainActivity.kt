93aa1cfc4fb574a57074d6094fcba6a3802d9862
MILE GUERRA <mi.guerra@duocuc.cl>
2025-10-22 21:05:03 -0300

    feat: se agrega creación de DataSeeder (datos base de la app)

--- app/src/main/java/com/example/quintaruta/MainActivity.kt
+++ app/src/main/java/com/example/quintaruta/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.quintaruta
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.quintaruta.data.local.AppDatabase
+import com.example.quintaruta.data.repository.TriviaRepository
+import com.example.quintaruta.data.seed.DataSeeder
 import com.example.quintaruta.ui.navigation.AppNavigation
 import com.example.quintaruta.ui.theme.QuintaRutaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val database = AppDatabase.getDatabase(applicationContext)
+        val triviaRepository = TriviaRepository(database.triviaDao())
+
+        DataSeeder.seedTrivias(triviaRepository)
+
         setContent {
             QuintaRutaTheme {
                 AppNavigation()
